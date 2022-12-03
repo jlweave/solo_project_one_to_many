a8e9874c3ef5991c73d72bb7d4661ad9ca669319
Lacey Weaver <jlweave89@gmail.com>
2022-12-03 16:15:44 -0700

    Add completed story six

--- spec/features/libraries/show_spec.rb
+++ spec/features/libraries/show_spec.rb
@@ -2,17 +2,20 @@ require "rails_helper"
 
 RSpec.describe "library show page" do
   before(:each) do
-    @library1 = Library.create!(city: "Denver", name:"Denver Public Library", computer_lab: true)
-    @library2 = Library.create!(city: "Colorado Spring", name:"Colorado Springs Public Library", computer_lab: false)
-    # @book1 = @library1.book.create!(name: "Fairy Tale", IBN: 9781668002179, available: true)
-    # @book2 = @library1.book.create!(name: "The Boy From Biloxi", IBN: 9780385548922, available: false)
-    # @book3 = @library2.book.create!(name: "Lessons in Chemisty", IBN: 9780385549400, available: true)
+    @library1 = Library.create!(city: "Denver", name:"Denver Public Library", computer_lab: true, phone_number: 7208651111)
+    @library2 = Library.create!(city: "Colorado Spring", name:"Colorado Springs Public Library", computer_lab: false, phone_number: 7195316333)
+    @book1 = @library1.books.create!(name: "Fairy Tale", IBN: 9781668002179, available: true)
+    @book2 = @library1.books.create!(name: "The Boy From Biloxi", IBN: 9780385548922, available: false)
+    @book3 = @library2.books.create!(name: "Lessons in Chemisty", IBN: 9780385549400, available: true)
+    @book4 = @library2.books.create!(name: "The Light We Carry: Overcoming in Uncertain Times", IBN: 9780593237465, available: false)
+    @book5 = @library2.books.create!(name: "If He Had Been with Me", IBN: 9781728205489, available: true)
   end
 
   describe 'user story 2' do
    describe "as a visitor When I visit '/parents/:id'" do
     it "Then I see the parent with that id including the parent's attributes" do
       visit "/libraries/#{@library1.id}"
+
       expect(page).to have_content(@library1.city)
       expect(page).to have_content(@library1.name)
       expect(page).to have_content(@library1.computer_lab)
