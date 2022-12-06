1abd396bf705d9b8e72a415a08261d4b8080851a
Lacey Weaver <jlweave89@gmail.com>
2022-12-06 07:21:45 -0700

    Add completed story twelve

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -14,4 +14,18 @@ class LibrariesController < ApplicationController
     library = Library.create(name: params[:name], city: params[:city], phone_number: params[:phone_number])
     redirect_to "/libraries/#{library.id}"
   end
+
+  def edit
+   @library = Library.find(params[:id])
+  end
+
+  def update
+    library = Library.find(params[:id])
+    library.update(library_params)
+    redirect_to '/libraries'
+  end
+  private
+  def library_params
+    params.permit(:name, :city, :phone_number, :computer_lab)
+  end
 end
